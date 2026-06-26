65eacbafb03b665fe564222a863e806e2115879d
Ian Wessels <ian_wessels@icloud.com>
2026-06-26 15:16:29 +0200

    fix sitemap canonical domain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.corporatecricketcollective.com/sitemap.xml",
+    sitemap: "https://corporatecricketcollective.com/sitemap.xml",
   };
 }
