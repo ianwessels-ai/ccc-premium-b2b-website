68baf43b02c87aa0110e3cd7265c738091ceab7c
Ian Wessels <ian_wessels@icloud.com>
2026-07-21 18:08:15 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,9 +35,7 @@ export default function Footer() {
               <Link href="/contact" className="hover:text-white">
                 Contact
               </Link>
-              <Link href="/privacy" className="hover:text-white">
-                Privacy Policy
-              </Link>
+              
               <Link href="/terms" className="hover:text-white">
                 Terms
               </Link>
