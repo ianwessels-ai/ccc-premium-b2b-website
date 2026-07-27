e10eb465ebd724cab33e4257381b9a0da42990e7
Ian Wessels <ian_wessels@icloud.com>
2026-07-27 17:14:05 +0200

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -26,7 +26,7 @@ export default function Nav() {
               width={160}
               height={46}
               priority
-              className="h-auto w-[135px] sm:w-[150px] lg:w-[160px]"
+              className="h-auto w-[170px] sm:w-[190px] lg:w-[215px]"
             />
           </Link>
 
