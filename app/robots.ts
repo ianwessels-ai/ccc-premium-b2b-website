a89f72dcfd9323075eac176f37d7a7ed89ba624c
Ian Wessels <ian_wessels@icloud.com>
2026-08-10 15:28:48 +0200

    Update to www

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://corporatecricketcollective.com/sitemap.xml",
+    sitemap: "https://www.corporatecricketcollective.com/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://corporatecricketcollective.com";
+  const baseUrl = "https://www.corporatecricketcollective.com";
 
   return [
     {
